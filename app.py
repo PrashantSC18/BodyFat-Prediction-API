65653d71307c5ea6ce78141bfb8ce30f01972738
PrashantSC18 <88611416+PrashantSC18@users.noreply.github.com>
2023-05-05 20:08:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def predict():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(debug=True)
+#     app.run(debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
